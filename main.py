f4c70c97cdec91c65c9c7958d11f21485bd8612b
WillthePill3 <wgroessmann01@bvsd.org>
2020-07-07 19:14:43 +0000

    edit

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
-print("Helloand welcome to my me")
-CustomerOrder=input("What type of taco do you like")
-print("great choice of ordering a" +CustomerOrder)
+class Dog:
+ def __init__(self, colorInput, breedInput, sizeInput):
+   self.bodycolor=colorInput
+   self.breedtypr=breedInput
+   self.size=sizeInput
+
+currentspeed=0
+speedlimit=100
+fueltanksize=100
+currentFuel=60
+mpg=20
+
+def increaseSpeed(self , speedChange):
+  input(currentspeed +10)
+  print("we are going faster")
+  if self.currentspeed > 100:
+   self.currentSpeed -=10
+   print("We are going to fast!")
+  else:
+   self.currentspeed == self.currentspeed
+   self.increaseSpeed + 10
+  if currentFuel <= 0:
+    return -1
+  elif currentspeed + speedchange > speedlimit:
+    return -2
+  else: 
+    return currentspeed + speedChange  
+    
+
+  
+
+
+
+
+ 
